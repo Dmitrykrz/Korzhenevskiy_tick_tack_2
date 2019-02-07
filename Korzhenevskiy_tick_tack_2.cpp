785155e36291d93b51e66e638a611bec6cc2c77e
Dmitrykrz <Dmitrykorzhenevskiy@gmail.com>
2019-02-07 21:50:02 +0300

    Add files via upload

---
+++ Korzhenevskiy_tick_tack_2.cpp
@@ -0,0 +1,89 @@
+﻿
+#include "Korzhenevskiy_tick_tack_2.h"
+
+
+int Korzhenevskiy_tick_tack_2::number_of_objects{};
+
+Korzhenevskiy_tick_tack_2* Korzhenevskiy_tick_tack_2::registry[Korzhenevskiy_tick_tack_2::len]{};
+
+Korzhenevskiy_tick_tack_2::Korzhenevskiy_tick_tack_2(fptr callback, uint32_t timer) {
+	registry[number_of_objects] = (this);
+	number_of_objects++;
+
+	_timer = timer;
+	this->callback = callback;
+	_enabled = false;
+	_lastTime = 0;
+	counts = 0;
+
+}
+
+Korzhenevskiy_tick_tack_2::~Korzhenevskiy_tick_tack_2() {}
+
+void Korzhenevskiy_tick_tack_2::start() {
+	_lastTime = millis();
+	_enabled = true;
+	}
+
+
+
+void Korzhenevskiy_tick_tack_2::stop() {
+	_enabled = false;
+	}
+
+
+void Korzhenevskiy_tick_tack_2::tick() {
+	if (_enabled) 
+	{
+
+		if ((millis() - _lastTime) >= _timer)
+		{
+			_lastTime = millis();
+			counts++;
+			callback();
+		}
+	}
+
+
+
+}
+
+
+void Korzhenevskiy_tick_tack_2::mass_start()
+{
+	for (int x = 0; x < number_of_objects; x++)
+		registry[x]->start();
+	
+}
+
+
+void Korzhenevskiy_tick_tack_2::mass_tick()
+{
+	for (int x = 0; x < number_of_objects; x++)
+		registry[x]->tick();
+
+}
+
+
+void Korzhenevskiy_tick_tack_2::mass_stop()
+{
+	for (int x = 0; x < number_of_objects; x++)
+		registry[x]->stop();
+
+}
+
+
+void Korzhenevskiy_tick_tack_2::setinterval(int timer) {
+	_timer = timer;
+}
+
+
+void Korzhenevskiy_tick_tack_2::restart() {
+	_lastTime = millis();
+
+}
+
+
+void Korzhenevskiy_tick_tack_2::zero_count() {
+		counts = 0;
+}
---
+++ Korzhenevskiy_tick_tack_2.h
@@ -0,0 +1,43 @@
+ 
+#include <stdint.h>
+#include "Arduino.h"
+#define max_number_of_timers 10
+
+typedef void(*fptr)();
+
+class Korzhenevskiy_tick_tack_2
+{
+
+	static constexpr int len = max_number_of_timers;
+	static int number_of_objects;
+	static Korzhenevskiy_tick_tack_2* registry[max_number_of_timers];
+
+public:
+
+	Korzhenevskiy_tick_tack_2(fptr callback, uint32_t timer);
+	~Korzhenevskiy_tick_tack_2();
+
+	void start();
+	void stop();
+	void tick();
+	void setinterval(int timer);
+	void restart();
+	void zero_count();
+	static void mass_start();
+	static void mass_tick();
+	static void mass_stop();
+	int counts;
+
+private:
+
+	bool _enabled;
+	uint32_t _timer;
+	fptr callback;
+	uint32_t _lastTime;
+
+};
+
+
+
+
+
